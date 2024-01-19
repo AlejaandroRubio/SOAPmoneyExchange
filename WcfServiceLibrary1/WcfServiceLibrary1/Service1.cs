5eb6993107d2b70ba652802b68d921b2a7dd50c8
AlejaandroRubio <102520782+AlejaandroRubio@users.noreply.github.com>
2024-01-19 10:23:42 +0100

    Commit 1

---
+++ WcfServiceLibrary1/WcfServiceLibrary1/Service1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WcfServiceLibrary1
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "Service1" en el código y en el archivo de configuración a la vez.
+    public class Service1 : IService1
+    {
+        private const decimal TazaDeCambioUSD_EUR = 1.10m;
+        private const decimal TazaDeCambioEUR_JPY = 163m;
+        private const decimal TazaDeCambioUSD_JPY = 149m;
+
+        public string GetData(string value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public decimal CambioDeMonedaRate(string MonedaOrigen, string MonedaDestino)
+        {
+
+            if (MonedaOrigen == "USD" && MonedaDestino == "EUR") { return TazaDeCambioUSD_EUR; }
+            else if (MonedaOrigen == "EUR" && MonedaDestino == "USD") { return TazaDeCambioUSD_EUR; }
+            else if (MonedaOrigen == "EUR" && MonedaDestino == "JPY") { return TazaDeCambioEUR_JPY; }
+            else if (MonedaOrigen == "JPY" && MonedaDestino == "EUR") { return TazaDeCambioEUR_JPY; }
+            else if (MonedaOrigen == "USD" && MonedaDestino == "JPY") { return TazaDeCambioUSD_JPY; }
+            else if (MonedaOrigen == "JPY" && MonedaDestino == "USD") { return TazaDeCambioUSD_JPY; }
+            else { return 0; }
+
+        }      
+        
+        public decimal CambioDeMoneda(string MonedaOrigen, string MonedaDestino, int Monto)
+        {
+            decimal MontoDeTazaDeCambio = CambioDeMonedaRate(MonedaOrigen, MonedaDestino);
+
+            if (MonedaOrigen == "EUR" && MonedaDestino == "USD")
+            {
+                return Monto * MontoDeTazaDeCambio;
+            }
+            else if (MonedaOrigen == "USD" && MonedaDestino == "EUR")
+            {
+                return Math.Round(Monto / MontoDeTazaDeCambio, 2);
+            }
+            else if (MonedaOrigen == "JPY" && MonedaDestino == "EUR")
+            {
+
+                return Math.Round(Monto / MontoDeTazaDeCambio, 2);
+            }
+            else if (MonedaOrigen == "EUR" && MonedaDestino == "JPY")
+            {
+                return Monto * MontoDeTazaDeCambio;
+            }
+            else if (MonedaOrigen == "USD" && MonedaDestino == "JPY")
+            {
+                return Monto * MontoDeTazaDeCambio;
+            }
+            else if (MonedaOrigen == "JPY" && MonedaDestino == "USD")
+            {
+                return Math.Round(Monto / MontoDeTazaDeCambio, 2);
+            }
+            else { return 0; }
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+    }
+}
---
+++ WcfServiceLibrary1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        enum Monedas
+        {
+            USD,
+            EUR,
+            JPY,
+        }
+
+
+        private void button1_Click(System.Object sender, System.EventArgs e)
+        {
+            ServiceReference2.Service1Client client = new
+                ServiceReference2.Service1Client();
+            decimal returnExchange;
+
+
+            string MonedaOrigen = DropDownBoxOrigen.Text;
+           string MonedaDestino = DropDownBoxDestino.Text;
+
+            returnExchange = client.CambioDeMoneda(MonedaOrigen, MonedaDestino, Convert.ToInt32(textBox1.Text));
+
+            MessageBox.Show("El monto a cambiar es "+textBox1.Text+" "+ MonedaOrigen+" a "+ MonedaDestino +" es: "+ returnExchange + "");
+            
+
+        }
+    }
+}
